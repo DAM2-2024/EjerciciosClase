1f4d5d8da6abd4f44eae4f1c181cfb2a5caab9c4
Profesor Vespertino <ProfVespertino@PROFESOR09.informatica.comercio>
2023-09-26 20:59:24 +0200

    Primer ejercicio alumnos Primos

---
+++ NumeroPrimoConsola/NumeroPrimoConsola/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string cadena;
+        int numero = int.MinValue;
+        do
+        {
+            Console.WriteLine("Escribe un número");
+            cadena = Console.ReadLine() ?? string.Empty;
+        } while (!int.TryParse(cadena, out numero));
+
+        int contador = 0;
+        for (int i = 1; i < Math.Ceiling(Math.Sqrt(numero)); i++)
+        {
+            if (numero % i == 0)
+            {
+                contador++;
+            }
+        }
+        if (contador == 1)
+        {
+            Console.WriteLine("Es primo");
+            return;
+        }
+        Console.WriteLine("No es primo");
+    }
+}
+
+
+
