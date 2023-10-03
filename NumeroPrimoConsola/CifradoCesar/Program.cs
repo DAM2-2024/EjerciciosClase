cb0d1efe3dca9b13a6927739c3448d8b59aa9b90
Your Name <you@example.com>
2023-10-03 19:26:00 +0200

    Ejercicios Cesar

--- NumeroPrimoConsola/CifradoCesar/Program.cs
+++ NumeroPrimoConsola/CifradoCesar/Program.cs
@@ -1,26 +1,34 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 class Program
 {
-    private const int DESPLAZAMIENTO= 3;
+    private const int DESPLAZAMIENTO = 3;
     static void Main(string[] args)
     {
-        Console.WriteLine("Escribe un mensaje");
-        string cadena= Console.ReadLine() ?? string.Empty;
-        cadena= cadena.ToUpper();
-        List<char> abecedario = Enumerable.Range('A', 'Z' - 'A' + 1).Select(c => (char)c).ToList();
+        string cadena = File.ReadAllText("el_quijote.txt");
+        List<char> caracteresDistintos = cadena.ToCharArray().Distinct().ToList();
+        Dictionary<char, int> diccionarioRepeticiones = new Dictionary<char, int>();
+        caracteresDistintos.ForEach(caracter =>
+        {
+            diccionarioRepeticiones.Add(caracter, cadena.Count(x => caracter == x));
+        });
+        diccionarioRepeticiones = diccionarioRepeticiones.OrderByDescending(z => z.Value).ToDictionary(x => x.Key, y => y.Value);
+        File.WriteAllLines("diccionario.txt", diccionarioRepeticiones.Select(x => $"{x.Key}:{x.Value}"));
+
+        List<char> abecedario = diccionarioRepeticiones.Keys.ToList();
         int indice = int.MinValue;
         StringBuilder builder = new StringBuilder();
         foreach (var caracter in cadena)
         {
-            indice= abecedario.IndexOf(caracter);
+            indice = abecedario.IndexOf(caracter);
             if (indice != -1)
             {
                 builder.Append(abecedario[(indice + DESPLAZAMIENTO) % abecedario.Count]);
             }
         }
-        Console.WriteLine($"El resultado es {builder}");
+        File.WriteAllText("textoCifrado.txt", builder.ToString());
     }
 }
 
