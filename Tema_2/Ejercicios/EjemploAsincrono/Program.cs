ab2ae52e1a3b80d78da9cae94cb71dd17d145efc
Your Name <you@example.com>
2023-10-09 10:48:01 +0200

    asincronia

---
+++ Tema_2/Ejercicios/EjemploAsincrono/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        // Llamamos a un método asíncrono
+        await DoSomethingAsync();
+
+        // Continuamos con otras tareas
+        Console.WriteLine("Otras tareas pueden continuar mientras se espera DoSomethingAsync");
+    }
+
+    static async Task DoSomethingAsync()
+    {
+        // Simulamos una operación que lleva tiempo (por ejemplo, una solicitud HTTP)
+        using (HttpClient httpClient = new HttpClient())
+        {
+            string result = await httpClient.GetStringAsync("https://www.example.com");
+            Console.WriteLine(result);
+        }
+    }
+}
