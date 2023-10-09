7d697927ec0144eaf99fd49f00e6e40c74b98ac3
Profesor Vespertino <ProfVespertino@PROFESOR09.informatica.comercio>
2023-10-09 17:35:38 +0200

    LinQ PROS

---
+++ Tema_2/Ejercicios/LINQMasters/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using System.Numerics;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        List<Persona> pipols = new();
+
+        Persona persona= new Persona();
+        persona.Nombre = "rafa";
+        persona.DNI = "166334A";
+        persona.Apellido = "jja";
+        pipols.Add(persona);
+
+        persona = new Persona();
+        persona.Nombre = "rafa";
+        persona.DNI = "166334B";
+        persona.Apellido = "dumb";
+        pipols.Add(persona);
+
+        persona = new Persona();
+        persona.Nombre = "rafa";
+        persona.DNI = "166334B";
+        persona.Apellido = "DOS";
+        pipols.Add(persona);
+
+        var resultado = from person in pipols
+                                 group person by person.DNI into gPerson
+                                 select new {Clave = gPerson.Key, Apellido=gPerson.Select(x=>x.Apellido).Last()};
+
+        var dicciorio =resultado.ToDictionary(x => x.Clave, x => x);
+
+        var error = pipols.ToDictionary(x => x.DNI, x => x.Apellido);
+
+        Console.WriteLine(resultado);
+    }
+
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+        public string Apellido { get; set; }
+    }
+}
+
